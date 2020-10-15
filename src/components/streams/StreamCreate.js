d7d313f26a6e69e04f3409c6129c96c2ac8755df
Chirag Jain <cjain8060@gmail.com>
2020-10-15 21:45:14 +0530

    Stream Create form complete with validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,24 +2,27 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-	renderInput({input,label}) {
-		
+	renderError({ error, touched }) {
+		if (touched && error) {
+			return (
+				<div className="ui error message">
+					<div className="header">{error}</div>
+				</div>
+			);
+		}
+	}
+	// formated renderInput to a arrow function to solve the this issue since it will be called inside from the field component the this keyword will have a different value and will not work for renderError
+	renderInput = ({ input, label, meta }) => {
+		const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
 		return (
-			<div className='field'>
+			<div className={className}>
 				<label>{label}</label>
-				<input {...input} />
+				<input {...input} autoComplete="off" />
+				{/* <div>{meta.error}	</div> */}
+				{this.renderError(meta)}
 			</div>
 		);
-		/* return (
-			<input onChange={formProps.input.onChange} value={formProps.input.value} />
-		);
-		SAME SYNTAX
-		return <input {...formProps.input}/> */
-	}
-
-	// onSubmit(event) {
-	// 	event.preventDefault();
-	// }
+	};
 
 	onSubmit(formValues) {
 		console.log(formValues);
@@ -28,17 +31,42 @@ class StreamCreate extends React.Component {
 	render() {
 		return (
 			<div>
-				<form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form'>
-					<Field name='title' component={this.renderInput} label='Enter the title'/>
-					<Field name='description' component={this.renderInput} label='Enter description'/>
-					<button className='ui primary button'>Submit</button>
+				<form
+					onSubmit={this.props.handleSubmit(this.onSubmit)}
+					className="ui form error"
+				>
+					<Field
+						name="title"
+						component={this.renderInput}
+						label="Enter title"
+					/>
+					<Field
+						name="description"
+						component={this.renderInput}
+						label="Enter description"
+					/>
+					<button className="ui primary button">Submit</button>
 				</form>
 			</div>
-		)
+		);
 	}
 }
+
+const validate = formValue => {
+	// these errors will be somehow magically(due to redux form) be there inside the component part in the Field component
+	const errors = {};
+	if (!formValue.title) {
+		errors.title = 'You must enter a title';
+	}
+	if (!formValue.description) {
+		errors.description = 'You must enter a description';
+	}
+	return errors;
+};
+
 // this reduxForm is exactly like the connect function like helping us to call the action creators and getting the form data
 export default reduxForm({
 	// this is the name of the form
 	form: 'streamCreate',
+	validate: validate,
 })(StreamCreate);
