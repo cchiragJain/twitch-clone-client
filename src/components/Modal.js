f0b54e50334b481b32693402157b2642a6731b63
Chirag Jain <cjain8060@gmail.com>
2020-10-20 12:49:02 +0530

    created the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,29 @@
 import React from 'react';
-// notice how we are importing ReactDOM as well here
 import ReactDOM from 'react-dom';
+
+import history from '../history';
 
 const Modal = props => {
 	// this takes in two arguments first is some jsx that we want to show on the screen
 	return ReactDOM.createPortal(
-		<div className="ui dimmer modals visible active">
-			<div className="ui standard modal visible active">I am a modal</div>
+		<div
+			onClick={() => history.push('/')}
+			className="ui dimmer modals visible active"
+		>
+			{/* we added stopPropagation to solve event bubbling issues */}
+			<div
+				onClick={event => event.stopPropagation()}
+				className="ui standard modal visible active"
+			>
+				<div className="header">Delete Stream</div>
+				<div className="content">
+					Are you sure you want to delete the stream?
+				</div>
+				<div className="actions">
+					<button className="ui primary button">Delete</button>
+					<button className="ui button">Cancel</button>
+				</div>
+			</div>
 		</div>,
 		document.querySelector('#modal')
 	);
