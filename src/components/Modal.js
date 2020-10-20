cf3b0bf8468501e1ffaf16c4a3fac1e9036c429d
Chirag Jain <cjain8060@gmail.com>
2020-10-20 13:45:12 +0530

    made the modal reusable

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import history from '../history';
-
 const Modal = props => {
 	// this takes in two arguments first is some jsx that we want to show on the screen
 	return ReactDOM.createPortal(
-		<div
-			onClick={() => history.push('/')}
-			className="ui dimmer modals visible active"
-		>
+		<div onClick={props.onDismiss} className="ui dimmer modals visible active">
 			{/* we added stopPropagation to solve event bubbling issues */}
 			<div
 				onClick={event => event.stopPropagation()}
 				className="ui standard modal visible active"
 			>
-				<div className="header">Delete Stream</div>
-				<div className="content">
-					Are you sure you want to delete the stream?
-				</div>
-				<div className="actions">
-					<button className="ui primary button">Delete</button>
-					<button className="ui button">Cancel</button>
-				</div>
+				<div className="header">{props.title}</div>
+				<div className="content">{props.content}</div>
+				<div className="actions">{props.actions}</div>
 			</div>
 		</div>,
 		document.querySelector('#modal')
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,26 @@
 import React from 'react';
+
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+	const actions = (
+		// Fragment is kind of like an invisible component we can use to help us to render two adjacent jsx elements
+		// we can shorten React.Fragment to just an empty bracket like <></> this instead of <React.Fragment></React.Fragment>
+		<React.Fragment>
+			<button className="ui button negative">Delete</button>
+			<button className="ui button">Cancel</button>
+		</React.Fragment>
+	);
 	return (
 		<div>
 			StreamDelete
-			<Modal />
+			<Modal
+				title="Delete Stream"
+				content="Are you sure you want to delete the stream?"
+				actions={actions}
+				onDismiss={() => history.push('/')}
+			/>
 		</div>
 	);
 };
